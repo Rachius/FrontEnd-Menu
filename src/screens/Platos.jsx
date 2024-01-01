41b3be3318ca0004bcd458f3ea1754a796d435f2
Franco Amado <francoamado7@gmail.com>
2024-01-01 16:44:15 -0300

    se agrega estructura de página de platos

--- src/screens/Platos.jsx
+++ src/screens/Platos.jsx
@@ -2,7 +2,76 @@ import React from 'react'
 
 const Platos = () => {
   return (
-    <div>Platos</div>
+    <div container-fluid>    
+      <h3>Platos</h3>
+          <div>
+          <div class="row d-flex justify-content-around">
+                <div class="col-sm-3">
+                  <div class="card">
+                    <div class="card-body row d-flex align-items-center justify-content-center">
+                      <h5 class="card-title row-6">Nombre del plato</h5>
+                      <h5 class="card-title row-6">Precio</h5>
+                      <p class="card-text col-12">Descripción del plato.</p>
+                      <a href="#" class="btn btn-primary col-8 " >Añadir al pedido</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="card">
+                    <div class="card-body row d-flex align-items-center justify-content-center">
+                      <h5 class="card-title row-6">Nombre del plato</h5>
+                      <h5 class="card-title row-6">Precio</h5>
+                      <p class="card-text col-12">Descripción del plato.</p>
+                      <a href="#" class="btn btn-primary col-8 " >Añadir al pedido</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="card">
+                    <div class="card-body row d-flex align-items-center justify-content-center">
+                      <h5 class="card-title row-6">Nombre del plato</h5>
+                      <h5 class="card-title row-6">Precio</h5>
+                      <p class="card-text col-12">Descripción del plato.</p>
+                      <a href="#" class="btn btn-primary col-8 " >Añadir al pedido</a>
+                    </div>
+                  </div>
+                </div>
+            </div>        
+            <br />
+              <div class="row d-flex justify-content-around">
+              <div class="col-sm-3">
+                  <div class="card">
+                    <div class="card-body row d-flex align-items-center justify-content-center">
+                      <h5 class="card-title row-6">Nombre del plato</h5>
+                      <h5 class="card-title row-6">Precio</h5>
+                      <p class="card-text col-12">Descripción del plato.</p>
+                      <a href="#" class="btn btn-primary col-8 " >Añadir al pedido</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="card">
+                    <div class="card-body row d-flex align-items-center justify-content-center">
+                      <h5 class="card-title row-6">Nombre del plato</h5>
+                      <h5 class="card-title row-6">Precio</h5>
+                      <p class="card-text col-12">Descripción del plato.</p>
+                      <a href="#" class="btn btn-primary col-8 " >Añadir al pedido</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="card">
+                    <div class="card-body row d-flex align-items-center justify-content-center">
+                      <h5 class="card-title row-6">Nombre del plato</h5>
+                      <h5 class="card-title row-6">Precio</h5>
+                      <p class="card-text col-12">Descripción del plato.</p>
+                      <a href="#" class="btn btn-primary col-8 " >Añadir al pedido</a>
+                    </div>
+                  </div>
+                </div>
+            </div>        
+        </div>
+      </div>
   )
 }
 
--- src/screens/Registro.jsx
+++ src/screens/Registro.jsx
@@ -3,24 +3,25 @@ import { Form } from 'react-router-dom'
 
 const Registro = () => {
   return (
-      <div class="d-flex justify-content-sm-center">
+      <div class="d-flex justify-content-center">
         <div >
           <br />
           <br />
             <h3>Formulario de registración</h3>
             <br />
             <form>
-              <div class="mb-3">
+              <div class="mb-3 needs-validation" novalidate>
               <label for="InputNameReg" class="form-label">Nombre</label>
-              <input type="text" class="form-control" id="InputNameReg" aria-describedby="nameregHelp" />
+              <input type="text" class="form-control" id="InputNameReg" aria-describedby="nameregHelp" required />
             </div>
             <div class="mb-3">
               <label for="InputEmailReg" class="form-label">Usuario</label>
-              <input type="email" class="form-control" id="InputEmailReg" aria-describedby="emailregHelp" placeholder="usuario@correo.com" />
+              <input type="email" class="form-control" id="InputEmailReg" aria-describedby="emailregHelp" placeholder="usuario@correo.com" required />
             </div>
             <div class="mb-3">
               <label for="InputPasswordReg" class="form-label">Contraseña</label>
-              <input type="password" class="form-control" id="InputPasswordReg" />
+              <input type="password" class="form-control" id="InputPasswordReg" required/>
+                    
             </div>
             {/* <div class="mb-3 form-check">
               <input type="checkbox" class="form-check-input" id="exampleCheck1" />
@@ -29,8 +30,12 @@ const Registro = () => {
             <div>         
             <button type="submit" class="btn btn-success">Submit</button>
             </div>
+
+
+            
             
           </form>
+          
           </div>
       </div>
   )
